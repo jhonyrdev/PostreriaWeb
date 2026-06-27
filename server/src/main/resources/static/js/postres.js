3fb413af2b86169a7832315a36d3721074a5a814
jhonyrd <jhonyrodri156@gmail.com>
2026-06-27 14:48:15 -0500

    fix for frontend

--- server/src/main/resources/static/js/postres.js
+++ server/src/main/resources/static/js/postres.js
@@ -1,34 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () { 
-
-  document.getElementById('combo_Postre').addEventListener('change', function () {
-    const categoria = this.value;
-    document.querySelectorAll('.grupo-productos').forEach(el => {
-      el.style.display = (categoria === 'todos' || el.classList.contains(categoria)) ? 'block' : 'none';
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("combo_Postre")
+    .addEventListener("change", function () {
+      const categoria = this.value;
+      document.querySelectorAll(".grupo-productos").forEach((el) => {
+        el.style.display =
+          categoria === "todos" || el.classList.contains(categoria)
+            ? "block"
+            : "none";
+      });
     });
-  });
-
 
   //supuestamente codigo para las prmociones que diga vencido o sin stock
-  document.querySelectorAll('.promo').forEach(promo => {
+  document.querySelectorAll(".promo").forEach((promo) => {
     const fechaExpira = new Date(promo.dataset.expira);
     const hoy = new Date();
-    const estado = promo.querySelector('.estado');
+    const estado = promo.querySelector(".estado");
 
     if (hoy > fechaExpira) {
-      promo.classList.add('border-danger');
-      estado.textContent = 'Promoción vencida';
-      estado.style.color = 'red';
+      promo.classList.add("border-danger");
+      estado.textContent = "Promoción vencida";
+      estado.style.color = "#b97a57";
     } else {
-      promo.classList.add('border-success');
-      estado.textContent = 'Promoción vigente';
-      estado.style.color = 'green';
+      promo.classList.add("border-success");
+      estado.textContent = "Promoción vigente";
+      estado.style.color = "green";
     }
   });
 
   /*<-- Validación básica --> */
-  document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Formulario enviado correctamente ");
-  });
-
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      alert("Formulario enviado correctamente ");
+    });
 });
