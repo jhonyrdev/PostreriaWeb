715d92e23ca0dadc78ac3ac7d70985e87bfada2c
Jhonny Rodriguez <Jhonnyrodri156@gmail.com>
2026-06-28 09:35:12 -0500

    Fix CI profile and port configuration

--- server/src/test/java/com/postreria/server/ServerApplicationTests.java
+++ server/src/test/java/com/postreria/server/ServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.postreria.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class ServerApplicationTests {
 
 	@Test
